8a8264932df84022843e1ed0a36cf5ae38f2ef13
Bruno Stefano <brunoastefano@gmail.com>
2025-01-05 16:16:18 -0300

    created kafka consumer

---
+++ src/kafka_consumer/main.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'quickstart-events', 
+    bootstrap_servers = ['localhost:9092'],
+    auto_offset_reset = 'earliest',
+    enable_auto_commit = True,
+    group_id = 'quickstart-events-consumer',
+    value_deserializer = lambda x : x.decode('utf-8')
+    )
+
+for message in consumer:
+    print(message.value)
